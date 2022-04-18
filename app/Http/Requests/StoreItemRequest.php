3b7fcfd79bfc8072c97c22225f0556ae8b52eb0e
a0ki-github <aoki.hotaka@gmail.com>
2022-04-18 21:22:51 +0900

    add: Itemsのnameにunique制約を追加

--- app/Http/Requests/StoreItemRequest.php
+++ app/Http/Requests/StoreItemRequest.php
@@ -24,7 +24,7 @@ class StoreItemRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required'
+            'name' => 'required|unique:items'
         ];
     }
 }
