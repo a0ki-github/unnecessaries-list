4e94d25c9b2ea793128216fba8874cf697f404d3
a0ki-github <aoki.hotaka@gmail.com>
2022-04-18 21:19:45 +0900

    add: ItemのIndexについて、コントローラとテンプレートを作成

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -15,7 +15,8 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        $items = Item::all();
+        return view('items.index', ['items' => $items]);
     }
 
     /**
---
+++ resources/views/items/index.blade.php
@@ -0,0 +1,4 @@
+<h1>Index Items</h1>
+@foreach ($items as $item)
+    <li>{{ $item->name }}</li>
+@endforeach
