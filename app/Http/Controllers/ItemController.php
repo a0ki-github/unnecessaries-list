e6825eff32b27c87402c24713601000d3307282c
a0ki-github <aoki.hotaka@gmail.com>
2022-04-26 07:17:37 +0900

    add: items@showのコントローラとテンプレートを追加

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -52,7 +52,7 @@ class ItemController extends Controller
      */
     public function show(Item $item)
     {
-        //
+        return view('items.show', ['item' => $item]);
     }
 
     /**
@@ -63,7 +63,7 @@ class ItemController extends Controller
      */
     public function edit(Item $item)
     {
-        //
+        return view('items.edit', ['item' => $item]);
     }
 
     /**
