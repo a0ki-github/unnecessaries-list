9847816f4303e681f34896ee75302434759cab36
a0ki-github <aoki.hotaka@gmail.com>
2022-05-22 19:54:14 +0900

    add: Commentの作成・編集・削除のルーティングを設定

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,3 +38,6 @@ Route::get('/logout', [App\Http\Controllers\LoginController::class, 'logout'])
 
 Route::resource('items', ItemController::class)
     ->except(['destroy']);
+Route::resource('items.comments', CommentController::class)
+    ->only(['store', 'update', 'destroy'])
+    ->shallow();
