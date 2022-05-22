2895a87ac13032512c40bf4d5c31397194923349
a0ki-github <aoki.hotaka@gmail.com>
2022-05-22 10:42:56 +0900

    add: Item詳細ページにComment一覧を追加

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -2,3 +2,10 @@
 <p>{{ $item->name}}</p>
 <a href="{{ route('items.edit', ['item' => $item->uuid]) }}">edit</a>
 <a href="{{ route('items.index') }}">back</a>
+<h3>Comments</h3>
+@foreach ($item->comments as $comment)
+    <p>
+        <span>{{ $comment->user->name }}</span>
+        <span>{{ $comment->updated_at }}</p></span>
+    <p>{{ $comment->content }}</p>
+@endforeach
