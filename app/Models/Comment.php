1beae7407710ea106acdf439159c5fe3f78a4ccc
a0ki-github <aoki.hotaka@gmail.com>
2022-05-22 10:39:01 +0900

    add: UserとCommentのリレーションを設定

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\UuidModel;
 use App\Models\Item;
+use App\Models\User;
 
 class Comment extends UuidModel
 {
@@ -19,5 +20,10 @@ class Comment extends UuidModel
     public function item()
     {
         return $this->belongsTo(Item::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
